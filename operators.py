ab2d743e842dedc43bd5620dc0e12bc149de9e75
jb <46815338+officialtech@users.noreply.github.com>
2019-02-21 19:06:49 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,119 @@
+########################################   OPERATORS  #####################################
+
+
+Operators will perform operation on operands
+In python, Arithmetic Operators follow BODMAS rule
+
+B ------ Brackets
+O ------ Orders
+D ------- Division
+M ------- Multiplication
+A ------- Addition
+S ------- Subtraction
+
+In Python, we have below given operators
+
+>>>1. Arithmetic Operators <<<
+>>>2. Relational Operators <<<
+>>>3. Logical Operators <<<
+>>>. Assignment Operator <<<
+>>>5. ShortHand Assignment Operators <<<
+>>>6. Bitwise Operators <<<
+>>>7. Membership Operators <<<
+>>>8. Identity Operator <<<
+
+
+**************************************** Arithmetic Operators *****************************
+
+# Addition
+>>> 100 + 200 
+300
+----------------------
+# Subtraction
+>>> 250 - 145
+105
+------------------------
+# Multiplication
+>>> 25 * 2
+50
+-----------------------------
+# Division
+>>> 25 / 3
+8.333333333333334
+-----------------------------------
+# Modulus
+>>> 10 % 3
+1
+-------------------------------------------
+# Floor Division
+>>> 25 // 4
+6
+------------------------------------------------------
+# Exponential
+>>> 5 ** 4
+625
+--------------------------------------------------------------
+
+#-##########################################################1######################################################################1#
+TO DO                     45 + 2 - 8 * 9 + 5 / 2 + 16 - 8 * 2 / 2 + 6 + 4 / 2 - 5
+#.###########################################################0#####################################################################5#
+
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ RELATIONAL OPERATORS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The relational Operators will find the relation between two(2) operands and results True and False.
+
+>>> 10 > 10
+False
+----------------------------------- Greater than
+>>> 10 >= 10
+True
+------------------------------------ Greater than or equall to
+>>> 10 < 5
+False
+------------------------------------- Less than
+>>> 10 != 5
+True
+------------------------------------- Is Not Equall
+>>> 10 == 6
+False
+------------------------------------- Is Equall
+>>> 808 <= 944
+False
+------------------------------------- Less than or equall to
+
+
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ASCII <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+American Standard Code for Information Interchange
+The ASCII values are 256 that are from 0 to 255.
+
+--------------- type this on your python interpreter ---------- ord('A')
+
+------------------ type this on your python interpreter -------- chr(65)
+
+
+
+
+>>> "a" > "A"
+True
+.........................
+>>> "a" > "z"
+False
+.........................
+>>> 'A' < 'j'
+True
+..........................
+>>> 'jb' > 'jin'
+False
+.........................
+>>> 'jb' > 'jb'
+False
+.........................
+>>> 'jin' >= 'jin'
+True
+.........................
+
