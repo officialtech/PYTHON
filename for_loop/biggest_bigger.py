7d9ffe2705b068f1677e63d43c1545627e17e028
jb <46815338+officialtech@users.noreply.github.com>
2019-03-13 07:19:46 +0530

    Update biggest_bigger.py

--- for_loop/biggest_bigger.py
+++ for_loop/biggest_bigger.py
@@ -3,7 +3,7 @@
 big_number = 0
 next_number = 0
 
-for x in range(5):
+for x in range(10):
     number = int(input("Number: "))
     
     if number > big_number:
@@ -16,3 +16,23 @@ for x in range(5):
             
 print("Biggest Number: ", big_number)
 print("Bigger Number", next_number)
+
+
+
+# explained
+'''
+big_number = 0 # 7 , 10
+next_number = 0 # 6 ,
+
+for x in range(10):
+    number = int(input("Number: ")) # 7 , 6 , 10
+    if number > big_number: #7 > 0 = True, 6 > 7 = False, 10 > 7 = True
+        next_number = big_number # 0 = 0, 6 = 7
+        big_number = number #7=0 big_number = 7 , 7 = 10 big_number 10
+
+    else:
+        if number > next_number:  # 6 > 0
+            next_number = number # 0 = 6 next_number = 6
+print("Biggest Number: ", big_number)
+print("Bigger Number", next_number)
+'''
